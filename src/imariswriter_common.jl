76e2a4f822c8b183c6d26c1dd0029f766be8984f
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2021-03-15 11:51:03 -0400

    Export type symbols

--- src/imariswriter_common.jl
+++ src/imariswriter_common.jl
@@ -1,5 +1,7 @@
 # Automatically generated using Clang.jl
 
+export bpConverterTypesC_Float
+export bpConverterTypesC_UInt8, bpConverterTypesC_UInt16, bpConverterTypesC_UInt32, bpConverterTypesC_UInt64
 const bpConverterTypesC_UInt8 = Cuchar # UInt8
 const bpConverterTypesC_UInt16 = UInt16
 const bpConverterTypesC_UInt32 = UInt32
@@ -11,6 +13,7 @@ const bpImageConverterCPtr = Ptr{bpImageConverterC} # Ptr{Nothing}
 const bpConverterTypesC_ProgressCallback = Ptr{Cvoid} # Ptr{Nothing}
 
 export bpConverterTypesC_DataType
+export bpConverterTypesC_UInt8Type, bpConverterTypesC_UInt16Type, bpConverterTypesC_UInt32Type, bpConverterTypesC_FloatType
 @enum bpConverterTypesC_DataType::UInt32 begin
     bpConverterTypesC_UInt8Type = 0
     bpConverterTypesC_UInt16Type = 1
@@ -29,8 +32,10 @@ struct bpConverterTypesC_ImageExtent
 end
 
 const bpConverterTypesC_ImageExtentPtr = Ptr{bpConverterTypesC_ImageExtent}
-
 export bpConverterTypesC_ImageExtentPtr 
+
+export bpConverterTypesC_Dimension
+export bpConverterTypesC_DimensionX, bpConverterTypesC_DimensionY, bpConverterTypesC_DimensionZ, bpConverterTypesC_DimensionC, bpConverterTypesC_DimensionT
 @enum bpConverterTypesC_Dimension::UInt32 begin
     bpConverterTypesC_DimensionX = 0
     bpConverterTypesC_DimensionY = 1
@@ -50,7 +55,7 @@ end
 
 const bpConverterTypesC_DimensionSequence5DPtr = Ptr{bpConverterTypesC_DimensionSequence5D}
 
-export bpConverterTypesC_DimensionSequence5D 
+export bpConverterTypesC_Size5D 
 struct bpConverterTypesC_Size5D
     mValueX::UInt32
     mValueY::UInt32
@@ -62,7 +67,7 @@ end
 const bpConverterTypesC_Size5DPtr = Ptr{bpConverterTypesC_Size5D}
 
 export bpConverterTypesC_Index5D
-struct bpConverterTypesC_Index5D
+mutable struct bpConverterTypesC_Index5D
     mValueX::UInt32
     mValueY::UInt32
     mValueZ::UInt32
@@ -97,8 +102,11 @@ export tCompressionAlgorithmType
     eCompressionAlgorithmLZ4 = 21
     eCompressionAlgorithmLShuffleLZ4 = 31
 end
+for i in instances(tCompressionAlgorithmType)
+    @eval export $(Symbol(i))
+end
 
-export tCompressionAlgorithmType 
+export bpConverterTypesC_Options
 struct bpConverterTypesC_Options
     mThumbnailSizeXY::UInt32
     mFlipDimensionX::Bool
@@ -109,6 +117,8 @@ struct bpConverterTypesC_Options
     mNumberOfThreads::UInt32
     mCompressionAlgorithmType::tCompressionAlgorithmType
     mDisablePyramid::Bool
+    bpConverterTypesC_Options(mThumbnailSizeXY, mFlipDimensionX, mFlipDimensionY, mFlipDimensionZ, mForceFileBlockSizeZ1, mEnableLogProgress, mNumberOfThreads, mCompressionAlgorithmType, mDisablePyramid = false) =
+        new(mThumbnailSizeXY, mFlipDimensionX, mFlipDimensionY, mFlipDimensionZ, mForceFileBlockSizeZ1, mEnableLogProgress, mNumberOfThreads, mCompressionAlgorithmType, mDisablePyramid)
 end
 
 const bpConverterTypesC_OptionsPtr = Ptr{bpConverterTypesC_Options}
@@ -117,6 +127,10 @@ export tCompressionAlgorithmType
 struct bpConverterTypesC_Parameter
     mName::bpConverterTypesC_String
     mValue::bpConverterTypesC_String
+end
+function bpConverterTypesC_Parameter(mName::String, mValue::String)
+    bpConverterTypesC_Parameter(Cstring(pointer(mName)),
+                                Cstring(pointer(mValue)))
 end
 
 export bpConverterTypesC_ParameterSection
