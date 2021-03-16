426fb3971ba155261a983be481a2040e6ad0301e
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2021-03-16 14:37:18 -0400

    Streamline Comments

--- test/SimpleTestProgram.jl
+++ test/SimpleTestProgram.jl
@@ -1,5 +1,6 @@
 module SimpleTestProgram
     using ImarisWriter
+    # Manually import shorthand types
     import ImarisWriter: Options, Size5D, Parameter, ParameterSection, DimensionSequence5D
     import ImarisWriter: Color, ColorInfo, ImageExtent, Index5D, TimeInfo
 
@@ -59,37 +60,58 @@ module SimpleTestProgram
     # C function pointer for callback
     const aProgressCallback = @cfunction(progressCallback, Nothing, (bpConverterTypesC_Float, bpConverterTypesC_UInt64, Ptr{Nothing}) )
 
+    # Callback Task
+    function callbackTask(cond, aCallbackUserData)
+        run = true
+        while(run)
+            try
+                #println("Waiting... $(cond.handle)")
+                wait(cond)
+                data = aCallbackUserData[]
+                progressCallbackActual(data)
+            catch
+                run = false
+                # rethrow()
+            end
+        end
+        close(cond)
+        println("Exiting callback loop for test $aTestIndex")
+    end
+
+    """
+        testConvert(aTestIndex = 0)
+
+        Test with progress callback. Complex.
+    """
     function testConvert(aTestIndex = 0)
-        cond = Base.AsyncCondition()
         println("testConvert($aTestIndex)")
+
+        # Setup callback
+        cond = Base.AsyncCondition()
         # println("This is the beginning $aTestIndex: $(cond.handle)")
         aCallbackUserData = Ref( bpCallbackData(cond.handle, 0.0, 0, aTestIndex, -5) )
         ptr = Base.unsafe_convert(Ptr{Nothing}, aCallbackUserData)
-        t = @async begin
-            run = true
-            while(run)
-                try
-                    #println("Waiting... $(cond.handle)")
-                    wait(cond)
-                    data = aCallbackUserData[]
-                    progressCallbackActual(data)
-                catch
-                    run = false
-                    # rethrow()
-                end
-            end
-            close(cond)
-            println("Exiting callback loop for test $aTestIndex")
-        end
+
+        # Callback task to receive callback from foreign thread
+        t = @async callbackTask(cond, aCallbackUserData)
+
+        #Start actual program, preserving cond and aCallbackUserData from garbage collection
         GC.@preserve cond aCallbackUserData testConvertActual(aTestIndex, aCallbackUserData)
 
-        # Clean up 
+        # Clean up callback
         progressCallbackActual(aCallbackUserData[])
         close(cond)
         t
     end
 
+    """
+        testConvertActual(aTestIndex = 0, aCallbackuserData = C_NULL)
+
+        Test program without progress callback. Simpler.
+    """
     function testConvertActual(aTestIndex = 0, aCallbackUserData = C_NULL)
+        ### Setup
+
         aDataType = UInt8
         aImageSize = Size5D(X = 512, Y = 512, Z = 32, C = 4, T = 2)
         vVoxelSizeXY = 1.4
@@ -114,6 +136,8 @@ module SimpleTestProgram
         aApplicationName = "simpleTestProgram.jl"
         aApplicationVersion = "1.0.0"
 
+        ### Create File and Handle
+
         vConverter = ImarisWriter.create(
             aDataType, aImageSize, aSample,
             aDimensionSequence, aBlockSize,
@@ -122,6 +146,8 @@ module SimpleTestProgram
             aProgressCallback, aCallbackUserData
         )
         ImarisWriter.checkErrors(vConverter)
+
+        ### Write Blocks
 
         vBlockSize = aBlockSize.mValueX * aBlockSize.mValueY * aBlockSize.mValueZ * aBlockSize.mValueC * aBlockSize.mValueT
         vData = Vector{UInt8}(undef, vBlockSize)
@@ -148,61 +174,23 @@ module SimpleTestProgram
             yield()
         end
 
-        #=
-          bpConverterTypesC_ParameterSection* vImageSection = &vParameterSections[0];
-  vImageSection->mName = "Image";
-  vImageSection->mValuesCount = 1;
-  vImageSection->mValues = &vUnitParameter;
-        =#
+        ### Write Metadata
+
         vNumberOfOtherSections = 1
         vNumberOfSections = vNumberOfOtherSections + aImageSize.mValueC
-        #vParameterSections = Vector{bpConverterTypesC_ParameterSection}(undef,vNumberOfSections)
         vParameterSections = Vector{ParameterSection}(undef,vNumberOfSections)
         vUnitParameter = Parameter("Unit", "um")
         vImageSection = ParameterSection("Image", vUnitParameter)
         vParameterSections[1] = vImageSection
-        #=
-  char vChannelNamesBuffer[1024]; // will this be enough?
-  char* vChannelNameBuffer = vChannelNamesBuffer;
-        =#
-        #vChannelNamesBuffer = Vector{Cuchar}(undef,1024)
-        #vChannelNameBuffer = Ptr(vChannelNamesBuffer)
-
-  #=
-  unsigned int vNumberOfParametersPerChannel = 3;
-  bpConverterTypesC_Parameter* vChannelParameters = malloc(aImageSize.mValueC * vNumberOfParametersPerChannel * sizeof(bpConverterTypesC_Parameter));
-  for (unsigned int vC = 0; vC < aImageSize.mValueC; ++vC) {
-    bpConverterTypesC_Parameter* vThisChannelParameters = &vChannelParameters[vNumberOfParametersPerChannel * vC];
-    vThisChannelParameters[0].mName = "Name";
-    vThisChannelParameters[0].mValue = vC == 0 ? "First channel" : vC == 1 ? "Second channel" : vC == 2 ? "Third channel" : "Other channel";
-    vThisChannelParameters[1].mName = "LSMEmissionWavelength";
-    vThisChannelParameters[1].mValue = "700";
-    vThisChannelParameters[2].mName = "OtherChannelParameter";
-    vThisChannelParameters[2].mValue = "OtherChannelValue";
-    bpConverterTypesC_ParameterSection* vChannelSection = &vParameterSections[vNumberOfOtherSections + vC];
-    int vChannelNameLength = sprintf(vChannelNameBuffer, "Channel %i", vC);
-    vChannelSection->mName = vChannelNameBuffer;
-    vChannelNameBuffer += vChannelNameLength + 1;
-    vChannelSection->mValues = vThisChannelParameters;
-    vChannelSection->mValuesCount = vNumberOfParametersPerChannel;
-  }
-  =#
         vNumberOfParametersPerChannel = 3
-        #vChannelParameters = Matrix{Parameter}(undef, vNumberOfParametersPerChannel, aImageSize.mValueC)
         for vC = 1:aImageSize.mValueC
             vChannelParameters = Vector{Parameter}(undef, vNumberOfParametersPerChannel)
             vChannelParameters[1] = Parameter("Name", vC == 0 ? "First channel" : vC == 1 ? "Second channel" : vC == 2 ? "Third channel" : "Other channel")
             vChannelParameters[2] = Parameter("LSMEmissionWavelength", "700")
             vChannelParameters[3] = Parameter("OtherChannelParameter", "OtherChannelValue")
-            # Need to convert vChannelParameters to a pointer
             vParameterSections[vNumberOfOtherSections + vC] = ParameterSection("Channel $vC", vChannelParameters)
         end
 
-    #=
-  bpConverterTypesC_Parameters aParameters;
-  aParameters.mValuesCount = vNumberOfSections;
-  aParameters.mValues = vParameterSections;
-    =#
         #aParameters = bpConverterTypesC_Parameters(vParameterSections, vNumberOfSections)
 
         vTimeInfos = Vector{TimeInfo}(undef,aImageSize.mValueT)
@@ -213,37 +201,6 @@ module SimpleTestProgram
         # Convert vTimesInfos to a pointer
         #aTimeInfoPerTimePoint = bpConverterTypesC_TimeInfos(vTimeInfos,  aImageSize.mValueT)
 
-#=
-  bpConverterTypesC_TimeInfo* vTimeInfos = malloc(aImageSize.mValueT * sizeof(bpConverterTypesC_TimeInfo));
-  for (unsigned int vT = 0; vT < aImageSize.mValueT; ++vT) {
-    vTimeInfos[vT].mJulianDay = 2458885; // 5 feb 2020
-    unsigned long long vSeconds = vT + 4 + 60 * (27 + 60 * 15); // 3:27.04 PM + 1 sec per time point
-    vTimeInfos[vT].mNanosecondsOfDay = vSeconds * 1000000000;
-  }
-  bpConverterTypesC_TimeInfos aTimeInfoPerTimePoint;
-  aTimeInfoPerTimePoint.mValuesCount = aImageSize.mValueT;
-  aTimeInfoPerTimePoint.mValues = vTimeInfos;
-=#
-
-#=
-  bpConverterTypesC_ColorInfo* vColorInfos = malloc(aImageSize.mValueC * sizeof(bpConverterTypesC_ColorInfo));
-  for (unsigned int vC = 0; vC < aImageSize.mValueC; ++vC) {
-    bpConverterTypesC_ColorInfo* vColor = &vColorInfos[vC];
-    vColor->mIsBaseColorMode = true;
-    vColor->mBaseColor.mRed = (vC % 3) == 0 ? 1 : 0;
-    vColor->mBaseColor.mGreen = (vC % 3) == 1 ? 1 : 0;
-    vColor->mBaseColor.mBlue = (vC % 3) == 2 ? 1 : 0;
-    vColor->mBaseColor.mAlpha = 1;
-    vColor->mColorTableSize = 0;
-    vColor->mOpacity = 0;
-    vColor->mRangeMin = 0;
-    vColor->mRangeMax = 255;
-    vColor->mGammaCorrection = 1;
-  }
-  bpConverterTypesC_ColorInfos aColorInfoPerChannel;
-  aColorInfoPerChannel.mValuesCount = aImageSize.mValueC;
-  aColorInfoPerChannel.mValues = vColorInfos;
-=#
         vColorInfos = Vector{ColorInfo}(undef, aImageSize.mValueC)
         for vC = 1:aImageSize.mValueC
             vColorInfos[vC] = ColorInfo(
@@ -258,38 +215,16 @@ module SimpleTestProgram
         # Make vColorInfos into a pointer
         #aColorInfoPerChannel = bpConverterTypesC_ColorInfos(vColorInfos, aImageSize.mValueC)
 
-#=  bool aAutoAdjustColorRange = true;
-=#
         aAutoAdjustColorRange = true
 
-#=  bpImageConverterC_Finish(vConverter,
-    &aImageExtent, &aParameters, &aTimeInfoPerTimePoint,
-    &aColorInfoPerChannel, aAutoAdjustColorRange);
-  CheckErrors(vConverter);
-=#
-        #=
-        bpImageConverterC_Finish(vConverter,
-            Ref(aImageExtent), Ref(aParameters), Ref(aTimeInfoPerTimePoint),
-            Ref(aColorInfoPerChannel), aAutoAdjustColorRange)
-        =#
         ImarisWriter.finish(vConverter,
             aImageExtent, vParameterSections, vTimeInfos,
             vColorInfos, aAutoAdjustColorRange
         )
         ImarisWriter.checkErrors(vConverter)
 
-#=  free(vTimeInfos);
-  free(vColorInfos);
-
-  free(vParameterSections);
-  free(vChannelParameters);
-=#
-
-#  bpImageConverterC_Destroy(vConverter);
-
         ImarisWriter.destroy(vConverter)
 
-        # println(aProgressCallback)
     end
 
     function __init__()
